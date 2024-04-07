f9867da7b5ce7e7b0022be24d1319af9de29c3fb
Alexey Seren <dehaekx@gmail.com>
2024-04-07 20:51:08 +0700

    add realization for setters and setters

--- file.cpp
+++ file.cpp
@@ -1,3 +1,11 @@
 #include "file.h"
 
-File::File() {}
+File::File(QString name, QString path):fileName(name), filePath(path) {}
+
+QString File::getFileName() {return fileName;}
+QString File::getFilePath() {return filePath;}
+qint64 File::getFileSize() {return fileSize;}
+
+void File::setFileName(QString name) {fileName = name;}
+void File::setFilePath(QString path) {filePath = path;}
+void File::setFileSize(qint64 size) {fileSize = size;}
