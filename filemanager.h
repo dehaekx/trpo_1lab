8a9bce7df183317bcca58f2be7dcad741edbc878
Alexey Seren <dehaekx@gmail.com>
2024-04-13 17:44:18 +0700

    Qobject + makros, add removeFile method, slot fileChangedSlot

--- filemanager.h
+++ filemanager.h
@@ -4,14 +4,20 @@
 #include <QObject>
 #include "File.h"
 
-class FileManager
+class FileManager: public QObject
 {
-public:
+    Q_OBJECT
+private:
     QVector<File*> files;
 
 public:
+    FileManager();
     void addFile(File* file);
+    void removeFile(File* file);
     void updateFile(QString fileName, qint64 fileSize);
+public slots:
+    void fileChangedSlot(File* file);
+
 };
 
 #endif // FILEMANAGER_H
