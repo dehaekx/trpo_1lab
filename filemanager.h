28f50bebee2b0e94a61e5135cdaf63882f7794d2
Alexey Seren <dehaekx@gmail.com>
2024-04-13 18:12:03 +0700

    add variable loger in filemanager

--- filemanager.h
+++ filemanager.h
@@ -4,13 +4,14 @@
 #include <QObject>
 #include "File.h"
 #include <QDebug>
+#include "loger.h"
 
 class FileManager: public QObject
 {
     Q_OBJECT
 private:
     QVector<File*> files;
-
+    Loger * loger;
 public:
     FileManager();
     void addFile(File* file);
