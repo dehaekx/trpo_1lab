a5fae2cb12f1e32337ce0e098d07cb840375d849
Alexey Seren <dehaekx@gmail.com>
2024-04-13 17:14:28 +0700

    redid constructor for class file

--- file.cpp
+++ file.cpp
@@ -1,6 +1,23 @@
 #include "file.h"
+File::File()
+{
+    this->fileName = QString("");
+    this->filePath = QString("");
+    this->fileSize = 0;
+}
 
-File::File(QString name, QString path): fileName(name), filePath(path), fileSize(0) {}
+File::File(QString name, QString path):
+    fileName(name), filePath(path), fileSize(0)
+{
+    QFileInfo *fileinfo = new QFileInfo(filePath);
+    if (fileinfo)
+    {
+        this->fileName = QString(fileinfo->fileName());
+        this->fileSize = fileinfo->size();
+
+    }
+    delete fileinfo;
+}
 
 QString File::getFileName() {return fileName;}
 QString File::getFilePath() {return filePath;}
@@ -9,3 +26,5 @@ qint64 File::getFileSize() {return fileSize;}
 void File::setFileName(QString name) {fileName = name;}
 void File::setFilePath(QString path) {filePath = path;}
 void File::setFileSize(qint64 size) {fileSize = size;}
+
+
--- file.h
+++ file.h
@@ -2,6 +2,7 @@
 #define FILE_H
 #include <QFile>
 #include <QString>
+#include <QFileInfo>
 
 
 class File : public QFile
@@ -10,14 +11,15 @@ private:
     QString fileName;
     QString filePath;
     qint64 fileSize;
-
 public:
     File(QString fileName, QString filePath);
 
+        // три геттера
     QString getFileName();
     QString getFilePath();
     qint64 getFileSize();
 
+        // три сеттера
     void setFileName(QString newName);
     void setFilePath(QString newPath);
     void setFileSize(qint64 newSize);
