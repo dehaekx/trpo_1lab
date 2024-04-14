e9a3e9b4eea35c35853075438039a25cefc52a6a
Alexey Seren <dehaekx@gmail.com>
2024-04-14 15:25:11 +0700

    create QFiles

--- main.cpp
+++ main.cpp
@@ -2,8 +2,14 @@
 #include<QTextStream>
 #include<QDebug>
 #include <conio.h>
+#include "file.h"
 
-int main()
-{
-    return 0;
+int main(int argc, char *argv[]) {
+    QCoreApplication a(argc, argv);
+
+    // Создаем объекты файлов и менеджера файлов
+    QFile file1("file1.txt");
+    QFile file2("file2.txt");
+
+    return a.exec();
 }
