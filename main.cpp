f3ed9a2b8ab6c413285b2c31742c4f9a27303271
Alexey Seren <dehaekx@gmail.com>
2024-04-26 20:47:03 +0700

    try to run and check changes

--- main.cpp
+++ main.cpp
@@ -21,27 +21,32 @@ int main(int argc, char *argv[]) {
 
     QString currentReposiroty = QDir::currentPath().section("/", 0, -2);
     qDebug() << "Direction: " << currentReposiroty;
-    QString test = currentReposiroty + "/trpo_1lab/test_files/test1.txt";
-    //QString test = currentFilePathProject + "/trpo_1lab/test_files/test2.docx";
-    //QString test = currentFilePathProject + "/trpo_1lab/test_files/test3.jpg";
+    QString test = currentReposiroty + "/trpo_1lab/test_files/";
+    // QString test = currentFilePathProject + "/trpo_1lab/test_files/test2.docx";
+    // QString test = currentFilePathProject + "/trpo_1lab/test_files/test3.jpg";
 
-    // int count_of_files;
-    // qDebug() << "Input counts of files which you want to track: ";
-    // cin >> count_of_files;
-    // for (int i = 0; i < count_of_files; i++)
-    // {
-    //     qDebug() << "Enter your file" << i << "name with extension: ";
-    //     QString name_of_file;
-    //     cin >> name_of_file;
-    //     test += name_of_file;
-    //     filemanager.addFile(test);
-    // }
+    int count_of_files;
+    qDebug() << "Input counts of files which you want to track: ";
+    cin >> count_of_files;
+    for (int i = 0; i < count_of_files; i++)
+    {
+        qDebug() << "Enter your file" << i << "name with extension: "; // test1.txt
+        QString name_of_file;
+        cin >> name_of_file;
+        test += name_of_file;
+        filemanager.addFile(test);
+        test = test.section("/", 0, -2);
+        test += "/";
+        //qDebug() << test << Qt::endl;
+    }
 
-    filemanager.addFile(test);
+    //filemanager.checkfiles();
+
+    // filemanager.addFile(test);
 
     while (true)
     {
-        std::this_thread::sleep_for( std::chrono::milliseconds(1000) );
+        std::this_thread::sleep_for( std::chrono::milliseconds(1500));
         filemanager.checkfiles();
     };
 
